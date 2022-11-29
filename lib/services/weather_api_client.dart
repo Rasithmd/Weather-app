1a6a693da1bdc0dad129299490ef42b2e2bf67b6
Rasith Mohamed <catchrasith@gmail.com>
2022-11-29 15:15:33 +0530

    update

--- lib/services/weather_api_client.dart
+++ lib/services/weather_api_client.dart
@@ -1,8 +1,6 @@
 import 'dart:convert';
-
 import 'package:http/http.dart' as http;
 import 'package:weather_app/model/weather_model.dart';
-import 'package:weather_app/share_preferance.dart';
 
 class WeatherApiClient {
   Future<Weather>? getCurrentWeather(String? location) async {
@@ -10,10 +8,7 @@ class WeatherApiClient {
         "https://api.openweathermap.org/data/2.5/weather?q=$location&appid=90c878506c4812802ac65ebe6ca9edf4&units=metric");
     var response = await http.get(endpoint);
     var body = jsonDecode(response.body);
-    if (response != null) {
-      WeatherInfoPreference().saveWeatherInfo(response);
-    }
-    print(Weather.fromJson(body).cityName);
+    // print(Weather.fromJson(body).cityName);
     return Weather.fromJson(body);
   }
 }
